af6e483c7e1a22ecb34de3fccddd834b23b822d0
Moara Miranda <moaramirandass@gmail.com>
2023-07-14 14:58:53 +0200

    Refactoring

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import Controls from "./controls.js";
 import Timer from "./timer.js";
 import Sound from "./sounds.js"
+import Events from "./events.js"
 import {
   buttonPlay,
   buttonPause,
@@ -26,49 +27,6 @@ const timer = Timer({
 });
 
 const sound = Sound()
-/***********************
- Events
-***********************/
 
+Events({ controls, timer, sound})
 
-buttonPlay.addEventListener("click", function () {
-  controls.play();
-  timer.countDown();
-  sound.pressButton()
-});
-
-buttonPause.addEventListener("click", function () {
-  controls.pause();
-  timer.hold();
-  sound.pressButton();
-
-});
-
-buttonStop.addEventListener("click", function () {
-  controls.reset();
-  timer.reset();
-  sound.pressButton();
-});
-
-buttonSoundOn.addEventListener("click", function () {
-  buttonSoundOn.classList.add("hide");
-  buttonSoundOff.classList.remove("hide");
-  sound.bgAudio.play()
-});
-
-buttonSoundOff.addEventListener("click", function () {
-  buttonSoundOn.classList.remove("hide");
-  buttonSoundOff.classList.add("hide");
-  sound.bgAudio.pause();
-
-});
-
-buttonSet.addEventListener("click", function () {
-  let newMinutes = controls.getMinutes();
-  if (!newMinutes) {
-    alert("Let's get it started! Add your minutes");
-    return;
-  }
-  timer.updateDisplay(newMinutes, 0);
-  timer.updateMinutes(newMinutes);
-});
--- js/sounds.js
+++ js/sounds.js
@@ -17,7 +17,7 @@ export default function () {
     kitchenTimer.play();
   }
 
-  bgAudio.loop;
+  bgAudio.loop = true;
 
   return {
     pressButton,
